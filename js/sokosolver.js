30acba288f04b4ca22c9512eeefbf5d201e4922d
ggorlen <gsgorlen@gmail.com>
2023-06-05 22:50:27 -0700

    Continue solver experimentation

--- js/sokosolver.js
+++ js/sokosolver.js
@@ -1,81 +1,84 @@
 /**
  * homebrew sokoban solver
  *
- * in progress...
- * - speed up goto by allowing its BFS validation to be overridden when called from sokosolver
- * 
- * resources:
- * - http://sokobano.de/wiki/index.php?title=Solver
+ * - is BFS the way to go?  (doesn't minimize number of moves)
+ *   what about a combo DFS and BFS?  hmm..
  */
 
  
 // solves a sokoban level if possible
-const solve = function (game) {
+let solve = function(game) {
 
   // set of visited positions
-  const visited = {};
-  visited[JSON.stringify(game.level)] = true;
+  let visited = {};
   
   // queue for BFS with current position as starting point
-  const queue = [game];
-
+  let queue = [];
+  queue.push(game);
+  
   // iterate while there are positions to inspect
   while (queue.length) {
   
     // dequeue a position
     game = queue.shift();
     
-    // set this position as visited
-    visited[JSON.stringify(game.level)] = true;
-
+    // return true if this position is solved
+    if (game.isFinished()) {
+      console.table(game.level);
+      return true;
+    }
+    // return false if the position was already visited
+    else if (visited[JSON.stringify(game.level)]) {
+      continue;
+    }
     console.table(game.level);
     
-    // return true if this position is solved
-    if (game.isFinished()) { 
-      return true; 
-    }
+    // set this position as visited
+    visited[JSON.stringify(game.level)] = true;
     
     // get list of possible pushes for this position
     possiblePushes = getPossiblePushes(game);
     
-    console.log("possible pushes: ");
     console.log(possiblePushes);
+    
+    // if there are no possible pushes for this position, continue
+    if ($.isEmptyObject(possiblePushes)) {
+      continue;
+    }
     
     // execute each possible push on a copy of the level
     for (let key in possiblePushes) {
-
-      // parse the square coordinates
-      key = JSON.parse(key);
+      if (possiblePushes.hasOwnProperty(key)) {
         
-      // get the string of moves for this position
-      const moves = possiblePushes[key];
-      
-      for (let i = 0; i < moves.length; i++) {
+        // get the string of moves for this position
+        let moves = possiblePushes[key];
+        let solved = false;
+        
+        for (let i = 0; i < moves.length; i++) {
+            
+          // make a copy of the position
+          //let gameCopy = this.level.map((a) => a.slice());
+          let gameCopy = $.extend(true, {}, game);
           
-        // make a copy of the position
-        //let gameCopy = this.level.map((a) => a.slice());
-        const gameCopy = $.extend(true, {}, game);
-        
-        // execute move
-        if (moves[i] === "u") {
-          gameCopy.goTo(key[0], key[1]+1);
-          gameCopy.move("u", false);
-        }
-        else if (moves[i] === "d") {
-          gameCopy.goTo(key[0], key[1]-1);
-          gameCopy.move("d", false);
-        }
-        else if (moves[i] === "l") {
-          gameCopy.goTo(key[0]+1, key[1]);
-          gameCopy.move("l", false);
-        }
-        else if (moves[i] === "r") {
-          gameCopy.goTo(key[0]-1, key[1]);
-          gameCopy.move("r", false);
-        }
-
-        // enqueue the new position if unvisited
-        if (!visited[JSON.stringify(gameCopy.level)]) {
+          // execute move
+          if (moves[i] === "u") {
+            gameCopy.goTo(key[0], key[1]+1);
+            gameCopy.move("u", false);
+          }
+          else if (moves[i] === "d") {
+            gameCopy.goTo(key[0], key[1]-1);
+            gameCopy.move("d", false);
+          }
+          else if (moves[i] === "l") {
+            gameCopy.goTo(key[0]+1, key[1]);
+            gameCopy.move("l", false);
+          }
+          else if (moves[i] === "r") {
+            gameCopy.goTo(key[0]-1, key[1]);
+            gameCopy.move("r", false);
+          }
+          
+          // enqueue the new position following the push
           queue.push(gameCopy);
         }
       }
@@ -88,41 +91,44 @@ const solve = function (game) {
 
 // return a hash of possible pushes in a given position in a game
 // key: x, y location of pushable box.  value: string of possible pushes as udlr
-const getPossiblePushes = function (game) {
+let getPossiblePushes = function(game) {
   
   // extract position from game object
-  const position = game.level;
+  let position = game.level;
+    
+  // find the player location for this position
+  let px = game.px; 
+  let py = game.py;
   
   // array to store x, y locations of possible pushes for this position
   // along with the direction(s) of the push(es) in udlr format
-  const pushLocations = {};
-  console.log(pushLocations);
+  let pushLocations = {};
   
   // queue to hold squares pending inspection
-  const queue = [];
+  let queue = [];
   
   // hash of visited squares and their parents
-  const visited = {};
+  let visited = {};
   
   // enqueue origin, set it as visited and set its parent to null
-  queue.push([game.px, game.py]);
-  visited[game.px + " " + game.py] = null;
+  queue.push([px, py]);
+  visited[px + " " + py] = null;
   
   // while there are still nodes left to visit...
   while (queue.length) {
       
-    // dequeue an item
-    const current = queue.shift();
+    // dequeue the first item
+    let current = queue.shift();
   
     // is this a pushable box?  if so, add it to the pushLocations list
     // don't enqueue it either way
     if (["$", "*"].indexOf(position[current[1]][current[0]]) >= 0) {
       
       // get the parent of the box to determine which direction we're trying to push from
-      const parent = visited[current[0] + " " + current[1]];
+      let parent = visited[current[0] + " " + current[1]];
 
       // create the key for the current position
-      const pushKey = JSON.stringify([current[0], current[1]]);
+      let pushKey = JSON.stringify([current[0], current[1]]);
       
       // try pushing left
       if (parent[0] > current[0] &&
@@ -166,7 +172,7 @@ const getPossiblePushes = function (game) {
       }
     }
     else {  // not evaluating a box, grab a list of available adjacent squares
-      const adjacent = game.getNeighbors(current[0], current[1], [" ", "*", "$", "."]);
+      let adjacent = game.getNeighbors(current[0], current[1], [" ", "*", "$", "."]);
       
       // for each unvisited adjacent square or box square, enqueue and set parent
       for (let i = 0; i < adjacent.length; i++) {
